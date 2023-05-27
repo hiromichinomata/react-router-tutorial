8cdf183e17319800db8febc8bac687f93adae571
Hiromichi NOMATA <git@hiromichinomata.com>
2023-05-27 16:20:13 +0900

    update: delete button

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,11 @@ import ErrorPage from "./error-page";
 import Contact, {
   loader as contactLoader,
 } from "./routes/contact";
+import EditContact, {
+  action as editAction,
+} from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
 
 const router = createBrowserRouter([
   {
@@ -22,10 +27,22 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
+      { index: true, element: <Index /> },
       {
         path: "contacts/:contactId",
         element: <Contact />,
         loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
       },
     ],
   },
